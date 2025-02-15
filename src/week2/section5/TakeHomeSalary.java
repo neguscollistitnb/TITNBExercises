5121da3fb81a418143cd97bb29689eb10ab8a814
Negus Collis <neguscollis@Neguss-MacBook-Pro.local>
2025-02-15 12:03:24 -0600

    Add Description

--- src/week2/section5/TakeHomeSalary.java
+++ src/week2/section5/TakeHomeSalary.java
@@ -8,3 +8,21 @@ public class TakeHomeSalary {
 
 
 }
+
+
+/*
+In average US Employees are obligated to pay around 35% in taxes if they are making $130000/year or more.
+
+To find 35% of $130,000, you can multiply 130,000 by 0.35.
+In mathematical terms:
+35% of $130,000 = 130,000 * 0.35
+So, 35% of $130,000 = $45,500
+Therefore, 35% of $130,000 is equal to $45,500.
+Write a program that stores the values of totalSalary and taxAmountDue with the most suitable data types. Then create a takeHomeAmount variable where the value is the difference between totalSalary and taxAmountDue. End the program with printing out the value of takeHomeAmount.
+____ totalSalary = ____;
+_____ taxAmountDue = ____;
+_____ takeHomeAmount = ____;
+
+System.out.println(takeHomeAmount);
+
+ */
